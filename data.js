1fa75a574a45ce0394239dab2bde824abad2a479
shaniya <shaniyak247@gmail.com>
2018-10-13 15:43:00 -0400

    amazing shay update 2

--- data.js
+++ data.js
@@ -2,4 +2,30 @@
 let message = "shay";
 console.log(message);
 
+let nummy = 8
+console.log(nummy)
+
+let doesLikebacon = true;
+console.log(doesLikebacon)
+
+let chicken = true;
+console.log(chicken)
+number = 6
+console.log(number)
+
+let numsisters = 2 
+console.log(numsisters)
+
+let string = "hi there"
+console.log(string)
+
+let seafood = false;
+console.log(seafood)
+
+string = "hello there"
+console.log(string)
+
+string = "happiness"
+console.log(string)
+
 // Type your code below!
