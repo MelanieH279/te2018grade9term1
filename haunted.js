a1c7a61ec301d8e73c949cbdf8fb215ac569695d
kendric Terry <terrykendric@gmail.com>
2018-11-10 12:30:38 -0500

    Update haunted.js

--- haunted.js
+++ haunted.js
@@ -21,12 +21,48 @@ if(enterHouse == "y" || enterHouse == "yes") {
   player.courage += 3;
   console.log(`${player.name} prepares their ${player.weapon} and opens the door...`);
   // continue the story
-console.log("It is dark!..... Will you turn on your flashlight??")
-if(turnFlashlight == "y" || turnFlashlight == "yes")
+    
+   //console.log("It is dark!..... Will you turn on your flashlight??")
+   let turnFlashlight = READLINE.question("It is dark!..... Will you turn on your flashlight?(yes or no) ");
+
+
+  if(turnFlashlight == "y" || turnFlashlight == "yes"){
+    console.log(" You receive 5 points of intellect!")
+    console.log("You spot a floating chair!")
+
+    let attack = READLINE.question("Do you want to attack the ghost? (yes or no) ");
+
+    if(attack == "y" || attack == "yes"){
+  player.courage += 5;
+  console.log("Stays to attack the ghost")
+  player.courage += 10;
+  player.strength += 10;
+
+    }
+
+    else{//player ran 
+    console.log("Lose 1 point of courage!")
+    player.courage -= 1;
+    console.log("Rush out of the house!")
+    }//what does this do
+
+  }
+  
+    
+ // continue the story
+
 } else {
   player.intellect++;
   console.log(`${player.name} decides not to open the door. However...`);
-  // continue the story
+console.log("You hear a footsteps from the outside of the house. You run inside breathing very heavy!")
+console.log("You get spooked by a rat on the ground...... ohhh nooo!!!")
+console.log("You yell!")
+player.courage -- ;
+console.log("You leave running because a ghost appears behind you......please do not catch me!!")
+
+
+
+
 
 }
 console.log("Thanks for playing!");
